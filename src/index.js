5d477a119007d841045f68b68fdcf37ad08a5344
Kate-13 <kate_boyko@mail.ru>
2020-10-20 22:42:15 +0300

    feat: implement check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = str;
+
+    for(let i=0; i < str.length; i++) {
+        for(let j=0; j < bracketsConfig.length; j++) {
+          let pair = bracketsConfig[j][0] + bracketsConfig[j][1];
+          brackets = brackets.replace(pair,'');
+        }
+      }
+      if(brackets.length === 0) {
+        return true;
+      } else {
+        return false;
+      }
 }
